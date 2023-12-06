1fae71427d27f449b0d938b9210d70d4cda328f1
Prince Sinha <princesinha@Princes-MacBook-Pro.local>
2023-12-06 08:13:07 +0530

    Project Done

---
+++ src/firebaseBaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA8hM0RiS6bjqsIvQLLH_zFpCVz3JPEVnk",
+  authDomain: "consult-buddy.firebaseapp.com",
+  projectId: "consult-buddy",
+  storageBucket: "consult-buddy.appspot.com",
+  messagingSenderId: "91706468843",
+  appId: "1:91706468843:web:ca87a7fd2c11f8567c91ae",
+  measurementId: "G-X6JNL0NQD7"
+  };export default firebaseConfig;
