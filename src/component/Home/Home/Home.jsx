3f92f2e17f292724151f36ce8766a81690c0775a
= <=>
2023-12-12 23:41:42 +0530

    Project Done

--- src/component/Home/Home/Home.jsx
+++ src/component/Home/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import About from '../About/About';
 import BrowserSupport from '../BrowserSupport/BrowserSupport';
-import BuildTools from '../BuildTools/BuildTools';
 import Contact from '../Contact/Contact';
 import Footer from '../Footer/Footer';
 import HappyClient from '../HappyClient/HappyClient';
